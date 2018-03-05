987a37e1ff865146b91f1c3cbdc6114fab5db58c
kirill-nikonorov <Kirill-nikonorov@mail.ru>
2018-03-05 18:08:13 +0300

    worked

---
+++ src/index.js
@@ -0,0 +1,11 @@
+function component() {
+    var element = document.createElement('div');
+
+    element.innerHTML = 'Hello';
+
+
+    return element;
+}
+
+document.body.appendChild(component());
+
